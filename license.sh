4451e26d4b9949e66763ed1d1a62287abf5e0719
Matthew Borger <matthew@borgernet.com>
2013-03-04 23:48:03 -0800

    Fixed matching pattern to only match #! at beginning of line and licensed the file itself.

--- license.sh
+++ license.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2013 Matthew Borger <matthew@borgernet.com>
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 if [ $# -lt 1 ]; then
 	echo "Usage: $0 FILENAME [LICENSE_TYPE]"
 	exit
@@ -83,14 +97,14 @@ case ${EXTENSION} in
 	sed -i '1i\/*' ${FILE}
 	;;
 	py)
-	sed -i '/#!/a """' ${FILE}
-	sed -i "/#!/a ${LICENSE}" ${FILE}
-	sed -i '/#!/a """' ${FILE}
-	sed -i '/#!/G' ${FILE}
+	sed -i '/^#!/a """' ${FILE}
+	sed -i "/^#!/a ${LICENSE}" ${FILE}
+	sed -i '/^#!/a """' ${FILE}
+	sed -i '/^#!/G' ${FILE}
 	;;
 	pl|sh)
 	INSERT=$(echo ${LICENSE} | sed -e 's/^/# /' -e 's/\\n/\\n# /g')
-	sed -i "/#!/a ${INSERT}" ${FILE}
-	sed -i '/#!/G' ${FILE}
+	sed -i "/^#!/a ${INSERT}" ${FILE}
+	sed -i '/^#!/G' ${FILE}
 	;;
 esac
